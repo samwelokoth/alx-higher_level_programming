33c88731b44d403525daec8a9222677a51129901
samwelokoth <samwelokoth8@gmail.com>
2023-11-28 13:31:05 +0000

    Print string

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+str = "Holberton School"
+#You can find the source code here
+#The output of the program should be:
+#3 times the value of str
+#followed by a new line
+#followed by the 9 first characters of str
+#followed by a new line
+#You are not allowed to use any loops or conditional statement
+#Your program should be maximum 5 lines long
+
+print(str*3)
+print(str[:9])
+
+
