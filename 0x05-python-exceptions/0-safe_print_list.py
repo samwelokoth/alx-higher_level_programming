984b48c1e7b49670a436a4e42543223ac2949c71
samwel Okoth <samwelOkoth8@gmail.com>
2023-12-18 09:04:21 -0600

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Write a function that prints x elements of a list.
+
+    Prototype: def safe_print_list(my_list=[], x=0):
+    my_list can contain any type (integer, string, etc.)
+    All elements must be printed on the same line followed by a new line.
+    x represents the number of elements to print
+    x can be bigger than the length of my_list
+    Returns the real number of elements printed
+    You have to use try: / except:
+    You are not allowed to import any module
+    You are not allowed to use len()
+'''
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1  
+        except IndexError:
+            pass
+        finally:
+            print("\n")
+
+    return count
+
+# Implementation of the list
+my_list = ['somethingstupid\n', 234, 23, 12, 54]
+elements_printed = safe_print_list(my_list, 3)        
+print("The number of elements printed are:", elements_printed)
+
+
