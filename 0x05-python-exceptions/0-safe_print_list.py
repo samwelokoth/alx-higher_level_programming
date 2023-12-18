ef6676552d92e47893f5d16a698857c76fa6689f
samwel Okoth <samwelOkoth8@gmail.com>
2023-12-18 09:48:38 -0600

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-#Write a function that prints x elements of a list.
+'''Write a function that prints x elements of a list.
 
-   # Prototype: def safe_print_list(my_list=[], x=0):
-    #my_list can contain any type (integer, string, etc.)
-    #All elements must be printed on the same line followed by a new line.
-    #x represents the number of elements to print
-    #x can be bigger than the length of my_list
-    #Returns the real number of elements printed
-    #You have to use try: / except:
-    #You are not allowed to import any module
-    #You are not allowed to use len()
+    Prototype: def safe_print_list(my_list=[], x=0):
+    my_list can contain any type (integer, string, etc.)
+    All elements must be printed on the same line followed by a new line.
+    x represents the number of elements to print
+    x can be bigger than the length of my_list
+    Returns the real number of elements printed
+    You have to use try: / except:
+    You are not allowed to import any module
+    You are not allowed to use len()
+'''
+
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
+    init_value = 0
+    try:
+        for i in range(x):
             print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    return num
-# Implementation of the list
-my_list = ['somethingstupid', 234, 23, 12, 54]
-elements_printed = safe_print_list(my_list, 3)        
+            init_value += 1
+    except IndexError:
+        pass
+    finally:
+        print("")
+    return init_value
+
+# Values for implementing the above
+my_list = [23, 24, 55, "jumbo"]
+elements_printed = safe_print_list(my_list,7)
 print("The number of elements printed are:", elements_printed)
+
+  
