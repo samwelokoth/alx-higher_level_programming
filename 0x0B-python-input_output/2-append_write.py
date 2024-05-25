15d0d9f066e1ef5e150a4c14a11cd24fb3018e9d
samwelokoth <samwelokoth8@gmail.com>
2024-05-25 05:44:22 -0400

    python-input_output redone

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 2-append_write.py
+""" File name : 4-append_write.py
+"""
+
+
+def append_write(filename="", text=""):
+    """append_write appends a string at the end of a text file (UTF8)
+    Args:
+        filename (str): Defaults to "".
+        text (str): text to add. Defaults to "".
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
