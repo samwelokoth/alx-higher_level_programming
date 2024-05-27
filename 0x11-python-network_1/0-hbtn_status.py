45afd2dbb51bb66586b3152179a0c414671f2614
samwelokoth <samwelokoth8@gmail.com>
2024-05-27 07:47:23 -0400

    python-network_1 redone

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches http://0.0.0.0:5050/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    url = "http://0.0.0.0:5050/status"
+    request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
