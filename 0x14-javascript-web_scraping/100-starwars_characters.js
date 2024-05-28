eb38b50c5674636c6abedfb01eb93af61fc3818e
samwelokoth <samwelokoth8@gmail.com>
2024-05-28 15:48:32 -0400

    javascript-web_scraping redone

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,47 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Please provide a Movie ID');
+  process.exit(1);
+}
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request.get(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const characters = body.characters;
+  if (!characters) {
+    console.error('Invalid Movie ID');
+    return;
+  }
+
+  const characterPromises = characters.map((characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request.get(characterUrl, { json: true }, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(body.name);
         }
       });
     });
-  }
+  });
+
+  Promise.all(characterPromises)
+    .then((names) => {
+      names.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
+
