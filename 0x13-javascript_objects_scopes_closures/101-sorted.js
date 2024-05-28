5c848761e9e0a4d420837e4a907587cd6fcf4b5a
samwelokoth <samwelokoth8@gmail.com>
2024-05-28 14:26:23 -0400

    javascript_objects_scopes_closures redone

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
+// 101-sorted.js
+
+const { dict } = require('./101-data');
+
+const sortedDict = {};
+
+for (const key in dict) {
+    const value = dict[key];
+    if (!sortedDict[value]) {
+        sortedDict[value] = [];
+    }
+    sortedDict[value].push(key);
 }
-console.log(newDict);
+
+console.log(sortedDict);
